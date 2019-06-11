c5760a9924720006e3e8a5bb8f37eccd254cb7c4
gnitsuk <mitchell.hockley@ge.com>
2019-06-11 17:40:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 
 const app = express();
 var port = process.env.PORT || 3000;
-app.get('/', (req, res) => res.send('Hello Mandy'));
+app.get('/', (req, res) => res.send('Hello Barry'));
 var m_server = app.listen(port, () => console.log('Server is running on port ' + port));
 
 var m_webSocketServer = new WebSocketServer( { server: m_server, autoAcceptConnections: true} );
