a567ce3eda4f1054ca9eeb39e87b34858b0169a8
gnitsuk <mitchell.hockley@ge.com>
2019-06-12 17:59:11 +0100

    Update Socketeer.js

--- Socketeer.js
+++ Socketeer.js
@@ -1,4 +1,4 @@
-//var WebSocketServer = require('ws').Server;
+var WebSocketServer = require('ws').Server;
 var ASCII_MESSAGE = { "ENUMERATE_CLIENTS": 0, "HELP": 1, "ID": 2, "IDENTIFY": 3 };
 
 function Socketeer(server, nKeepAliveCheckInterval)
