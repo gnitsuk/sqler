0beb20a69b78c0c92fffccb05864fde7f400c92b
gnitsuk <mitchell.hockley@ge.com>
2019-06-12 17:57:52 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var WebSocketServer = require('ws').Server;
+//var WebSocketServer = require('ws').Server;
 var url = require('url');
 var path = require('path');
 var request = require('request');
@@ -8,10 +8,10 @@ var GroupDrawer = require('./GroupDrawer');
 
 const app = express();
 var port = process.env.PORT || 3000;
-app.get('/', (req, res) => res.send('Hello RUGGA'));
+app.get('/', (req, res) => res.send('Hello HERT'));
 var m_server = app.listen(port, () => console.log('Server is running on port ' + port));
 
-var m_webSocketServer = new WebSocketServer( { server: m_server, autoAcceptConnections: true} );
+//var m_webSocketServer = new WebSocketServer( { server: m_server, autoAcceptConnections: true} );
 
 var groupDrawer = new GroupDrawer();
 
@@ -23,10 +23,10 @@ socketeer.AddAdditionalHelpStringsCallback(groupDrawer.GetHelpStrings, groupDraw
 socketeer.AddNewClientConnectCallback(groupDrawer.HandleNewClientConnect, groupDrawer);
 socketeer.AddClientDisconnectCallback(groupDrawer.HandleClientDisconnect, groupDrawer);
 
-//socketeer.StartServer();
+socketeer.StartServer();
 
-m_webSocketServer.on('connection', function (ws)
-                                            {
-                                                ws.send("Server connection accepted. Client ID = KJH");
-                                                }
-);
+//m_webSocketServer.on('connection', function (ws)
+ //                                           {
+  //                                              ws.send("Server connection accepted. Client ID = KJH");
+   //                                             }
+//);
