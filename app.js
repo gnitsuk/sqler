8726162a0a32f8707ada3a157931d3577d31f3c4
gnitsuk <mitchell.hockley@ge.com>
2019-06-12 11:24:29 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,18 +3,21 @@ var url = require('url');
 var path = require('path');
 const express = require('express');
 var Socketeer = require('./Socketeer');
+var groupDrawer = new GroupDrawer();
 
 const app = express();
 var port = process.env.PORT || 3000;
-app.get('/', (req, res) => res.send('Hello Lederer'));
+app.get('/', (req, res) => res.send('Hello Nelly'));
 var m_server = app.listen(port, () => console.log('Server is running on port ' + port));
 
 var m_webSocketServer = new WebSocketServer( { server: m_server, autoAcceptConnections: true} );
+
+var groupDrawer = new GroupDrawer();
 
 var socketeer = new Socketeer(m_server, 20000);
 
 m_webSocketServer.on('connection', function (ws)
                                             {
-                                                ws.send("Server connection accepted. Client ID = ALEPH NULL " +  socketeer.Test());
+                                                ws.send("Server connection accepted. Client ID = ALEPH NULLY " +  socketeer.Test());
                                             }
 );
