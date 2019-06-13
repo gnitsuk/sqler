f97b194125924c0f3029850980fb374c8fb43412
gnitsuk <mitchell.hockley@ge.com>
2019-06-13 10:53:02 +0100

    Update Socketeer.js

--- Socketeer.js
+++ Socketeer.js
@@ -61,6 +61,7 @@ Socketeer.prototype.AddClient = function(ws)
                          }
                          else
                          {
+                             ws.send("HELLO");
                             this.m_socketeer.ProcessASCIIMessage.call(this.m_socketeer, this, message);
                          }     
                      }
