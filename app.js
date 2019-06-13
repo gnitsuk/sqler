830e082aeaf1166c0db5f7e5ef40b61833fe1a9f
gnitsuk <mitchell.hockley@ge.com>
2019-06-13 10:04:09 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var GroupDrawer = require('./GroupDrawer');
 
 const app = express();
 var port = process.env.PORT || 3000;
-app.get('/', (req, res) => res.send('Hello HERT'));
+app.get('/', (req, res) => res.send('Hello HEART'));
 var m_server = app.listen(port, () => console.log('Server is running on port ' + port));
 
 //var m_webSocketServer = new WebSocketServer( { server: m_server, autoAcceptConnections: true} );
