eb6638b6689eed00df34fbcc54d59715bf92261d
Hockley <105043131@LOGON.DS.GE.COM>
2020-07-08 10:21:07 +0100

    sql11

--- Chatterer.js
+++ Chatterer.js
@@ -1,3 +1,5 @@
+var SQL = require('./sql');
+
 var OP_CODES = { "NUM_OTHER_CLIENTS": 1 };
 var CHATTERER_ASCII_MESSAGE = { "ACTIVE_CLIENTS": 0 };
 
@@ -20,7 +22,7 @@ Chatterer.prototype.GetHelpStrings = function ()
 
 Chatterer.prototype.SQLTest = function()
 {
-    return "Dave";
+    return "Pete";
 }
 
 Chatterer.prototype.HandleASCIIMessage = function (szMessage, ws, clients)
