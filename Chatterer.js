78e1fd657dfce49b8afdac305c9f414b6c5ff84f
Hockley <105043131@LOGON.DS.GE.COM>
2020-07-08 10:11:38 +0100

    sql8

--- Chatterer.js
+++ Chatterer.js
@@ -22,7 +22,7 @@ Chatterer.prototype.GetHelpStrings = function ()
 
 Chatterer.prototype.SQLTest = function()
 {
-    return "Word Wide Web";
+    return "Word Wide Web 2";
 }
 
 Chatterer.prototype.HandleASCIIMessage = function (szMessage, ws, clients)
