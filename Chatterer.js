729f987776e3aeea5b9e1e07bd22b87bcfa5f7e4
Hockley <105043131@LOGON.DS.GE.COM>
2020-07-10 16:00:26 +0100

    abcdefghijk

--- Chatterer.js
+++ Chatterer.js
@@ -3,7 +3,7 @@ var SQL = require('./sql');
 var OP_CODES = { "NUM_OTHER_CLIENTS": 1 };
 var CHATTERER_ASCII_MESSAGE = { "ACTIVE_CLIENTS": 0 };
 
-function Chatterer()
+function Chatterer(router)
 {
     this.m_clients = {};
     this.m_numClients = 0;
@@ -14,7 +14,7 @@ function Chatterer()
                                             "Returns the number of clients in the group session."
     ];
 
-    this.m_sql = new SQL();
+    this.m_sql = new SQL(router);
 }
 
 Chatterer.prototype.GetHelpStrings = function ()
