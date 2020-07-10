b0b2807bfa46a2c2aa2e0b6fbf5b5bc2cacb98af
Hockley <105043131@LOGON.DS.GE.COM>
2020-07-10 16:42:12 +0100

    abcdefghijklmnopqrstu

--- sql.js
+++ sql.js
@@ -44,7 +44,7 @@ function sql(router)
 
             //this.m_szText = "222: " + Object.getOwnPropertyNames(result).toString();
 
-            this.m_szText = "IS: " + result['recordset'].length;
+            this.m_szText = "IS: " + + Object.getOwnPropertyNames(result['recordset']) + "\n" + result['recordset'].length;
 
 
 
