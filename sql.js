5797452baa63ce4f338daeef822d4056e3bdf45f
Hockley <105043131@LOGON.DS.GE.COM>
2020-07-10 15:29:53 +0100

    ab

--- sql.js
+++ sql.js
@@ -26,9 +26,9 @@ function sql(router)
     (async () => {
         try {
             // make sure that any items are correctly URL encoded in the connection string
-            //await sql.connect(config);
-            //const result = await sql.query`SELECT * from dbo.Persons`;
-            this.m_szText = config.toString();
+            await sql.connect(config);
+            const result = await sql.query`SELECT * from dbo.Persons`;
+            this.m_szText = "Maybe";
         } catch (err) {
             this.m_szText = "k";
         }
