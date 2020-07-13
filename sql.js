8bd4d697d0345d9332249b52e8fee262ce52bfed
Hockley <105043131@LOGON.DS.GE.COM>
2020-07-13 15:13:59 +0100

    new week 7232

--- sql.js
+++ sql.js
@@ -28,6 +28,48 @@ sql.prototype.GetDBRecords = function ()
     };
 
     (async () => {
+        //var err;
+        //var szResult = "";
+        try {
+
+            /*var conn = await mssql.connect(config);
+            var request = new mssql.Request();
+            const result = await mssql.query(`SELECT * from dbo.Persons`);
+            await conn.close();
+
+
+            var table = result['recordset'].toTable();
+
+            var nNumRecords = table.rows.length;
+
+            szResult = "Server:\n";
+            szResult += "Call : " + this.m_call + "\n";
+            szResult += "Content of dbo.Persons:\n";
+            szResult += "Fields = " + Object.getOwnPropertyNames(result['recordset'][0]) + "\n";
+            szResult += "Number Records = " + nNumRecords + "\n";
+
+            for (var nRecord = 0; nRecord < nNumRecords; nRecord++) {
+
+                szResult += table.rows[nRecord];
+
+                szResult += "\n";
+            }
+
+            szResult += "\n\n\n\n";*/
+
+            //Object.getOwnPropertyNames(result['recordset']) + "\n" + result['recordset'].length + "\n" + result['recordset'].toTable().columns.length + "\n" + Object.getOwnPropertyNames(result['recordset'][0]) + "\n" + result['recordset'][0].FirstName;
+
+            this.m_call++;
+
+            this.m_szLastDBQueryResult = this.m_call;
+
+        }
+        catch (err) {
+            this.m_szLastDBQueryResult = "Error Querying Database\n\n\n\n";
+        }
+    })();
+
+    /*(async () => {
         var err;
         var szResult = "";
         try {
@@ -68,7 +110,7 @@ sql.prototype.GetDBRecords = function ()
         {
             this.m_szLastDBQueryResult = "Error Querying Database\n\n\n\n";
         }
-    })();
+    })();*/
 
     return this.m_szLastDBQueryResult;
 }
