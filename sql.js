b19edcac872ddbc00d6ca1cd6c83b62bb55ea280
Hockley <105043131@LOGON.DS.GE.COM>
2020-07-13 10:09:44 +0100

    new week 1

--- sql.js
+++ sql.js
@@ -34,8 +34,10 @@ function sql()
 
             this.m_szText = "Server:\n";
             this.m_szText += "Content of dbo.Perosns:\n";
-            this.m_szText += "Fields = " + Object.getOwnPropertyNames(result['recordset'][0]);
+            this.m_szText += "Fields = " + Object.getOwnPropertyNames(result['recordset'][0]) + "\n";
             this.m_szText += "# records = " + result['recordset'].length + "\n";
+
+            this.m_szText += "\n\n\n\n";
 
             //+ Object.getOwnPropertyNames(result['recordset']) + "\n" + result['recordset'].length + "\n" + result['recordset'].toTable().columns.length + "\n" + Object.getOwnPropertyNames(result['recordset'][0]) + "\n" + result['recordset'][0].FirstName;
 
