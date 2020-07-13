91c0fa21672779769a317f45654c7dcd3775a0f0
Hockley <105043131@LOGON.DS.GE.COM>
2020-07-13 12:20:24 +0100

    new week 109

--- sql.js
+++ sql.js
@@ -27,41 +27,40 @@ sql.prototype.SetDatabaseContent = function (err)
     };
 
     (async () => {
-        try {
+                    try {
 
-            await mssql.connect(config);
-            var request = new mssql.Request();
-            const result = await mssql.query(`SELECT * from dbo.Persons`);
+                        await mssql.connect(config);
+                        var request = new mssql.Request();
+                        const result = await mssql.query(`SELECT * from dbo.Persons`);
 
-            var table = result['recordset'].toTable();
+                        var table = result['recordset'].toTable();
 
-            var nNumRecords = table.rows.length;
+                        var nNumRecords = table.rows.length;
 
-            this.m_szLastDBQueryResult = "Server:\n";
-            this.m_szLastDBQueryResult += "Content of dbo.Persons:\n";
-            this.m_szLastDBQueryResult += "Fields = " + Object.getOwnPropertyNames(result['recordset'][0]) + "\n";
-            this.m_szLastDBQueryResult += "Num. Records = " + nNumRecords + "\n";
+                        this.m_szLastDBQueryResult = "Server:\n";
+                        this.m_szLastDBQueryResult += "Content of dbo.Persons:\n";
+                        this.m_szLastDBQueryResult += "Fields = " + Object.getOwnPropertyNames(result['recordset'][0]) + "\n";
+                        this.m_szLastDBQueryResult += "Num. Records = " + nNumRecords + "\n";
 
-            for (var nRecord = 0; nRecord < nNumRecords; nRecord++) {
-                //var nNumCells = table.rows[nRecord].cells.length;
+                        for (var nRecord = 0; nRecord < nNumRecords; nRecord++) {
 
-                this.m_szLastDBQueryResult += table.rows[nRecord];
+                            this.m_szLastDBQueryResult += table.rows[nRecord];
 
-                this.m_szLastDBQueryResult += "\n";
-            }
+                            this.m_szLastDBQueryResult += "\n";
+                        }
 
-            this.m_szLastDBQueryResult += "\n\n\n\n";
+                        this.m_szLastDBQueryResult += "\n\n\n\n";
 
-            //+ Object.getOwnPropertyNames(result['recordset']) + "\n" + result['recordset'].length + "\n" + result['recordset'].toTable().columns.length + "\n" + Object.getOwnPropertyNames(result['recordset'][0]) + "\n" + result['recordset'][0].FirstName;
-
+                        //+ Object.getOwnPropertyNames(result['recordset']) + "\n" + result['recordset'].length + "\n" + result['recordset'].toTable().columns.length + "\n" + Object.getOwnPropertyNames(result['recordset'][0]) + "\n" + result['recordset'][0].FirstName;
 
 
-        }
-        catch (err)
-        {
-            this.m_szLastDBQueryResult = "Error Querying Database\n\n\n\n";
-        }
-    })();
+
+                    }
+                    catch (err)
+                    {
+                        this.m_szLastDBQueryResult = "Error Querying Database\n\n\n\n";
+                    }
+                })();
 }
 
 sql.prototype.GetDBRecords = function ()
